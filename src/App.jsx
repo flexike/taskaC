bf01501b0d5747748b4355e5b2dc494f6fbead52
flexike <flexike@ukr.net>
2024-07-18 19:02:44 +0300

    +parallax from 0

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,30 @@ import grassFront from "./assets/grass-front.svg";
 import logo from "./assets/logo.svg";
 
 function App() {
+  let grass_car = document.getElementsByClassName("carGrass");
+  let grass_front = document.getElementsByClassName("grassFront");
+  let sectionWindow = document.getElementsByTagName("section");
+
+  let xValue = 0,
+    yValue = 0;
+
+  window.addEventListener("mousemove", (e) => {
+    xValue = e.clientX - window.innerWidth / 2; // target center of screen X axis
+    yValue = e.clientY - window.innerHeight / 2; // target center of screen Y axis
+    // console.log(xValue, yValue);
+
+    let speedxCar = grass_car[0].dataset.speedx;
+    let speedyCar = grass_car[0].dataset.speedy;
+    let speedxGrass = grass_front[0].dataset.speedx;
+
+    grass_car[0].style.transform = `translateX(calc(${
+      xValue * speedxCar
+    }px)) translateY(calc(${yValue * speedyCar}px))`;
+    grass_front[0].style.transform = `translateX(calc(${
+      -xValue * speedxGrass
+    }px))`;
+  });
+
   return (
     <section>
       {/* imgs */}
@@ -45,8 +69,19 @@ function App() {
           <div className="btn-wrapper">
             <a href="">Signup</a>
           </div>
-          <img src={carGrass} className="carGrass" alt="1" />
-          <img src={grassFront} className="grassFront" alt="2" />
+          <img
+            src={carGrass}
+            className="carGrass"
+            data-speedx="0.09"
+            data-speedy="0.03"
+            alt="3"
+          />
+          <img
+            src={grassFront}
+            className="grassFront"
+            data-speedx="0.03"
+            alt="4"
+          />
         </main>
       </div>
     </section>
